98754d1ee5bee602a71d9bc592357c7b4edceabd
msy7822-ux <masa.1108@icloud.com>
2020-09-29 20:13:06 +0900

    display error message

--- resources/views/commons/layout.blade.php
+++ resources/views/commons/layout.blade.php
@@ -17,6 +17,16 @@
         </div>
     </header>
 
+@if ($errors->any())
+    <div class="card-text text-left alert alert-danger">
+    <ul class="mb-0">
+        @foreach($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+    </div>
+@endif
+
     <div>
         @yield('content')
     </div>
